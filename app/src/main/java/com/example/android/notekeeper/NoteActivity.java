ef75b3a97a599e16c880fa7d264c4b73a531875c
elducati <geomondi09@gmail.com>
2018-11-03 12:39:13 +0300

    add cancel to menu_note

--- app/src/main/java/com/example/android/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/android/notekeeper/NoteActivity.java
@@ -25,7 +25,7 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteTitle;
     private EditText mTextNoteText;
     private int mNotePosition;
-
+    private boolean mIsCancelling;
 
 
     @Override
@@ -55,7 +55,13 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        saveNote();
+        if (mIsCancelling) {
+            if(mIsNewNote) {
+                DataManager.getInstance().removeNote(mNotePosition);
+            }
+        } else {
+            saveNote();
+        }
     }
 
     private void saveNote() {
@@ -106,6 +112,9 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
+        }else if (id == R.id.action_cancel) {
+            mIsCancelling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
